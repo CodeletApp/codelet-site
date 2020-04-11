d30ea0d228c3641fbac33c90e22e9326d8327cda
Mark <markgoldstein12@gmail.com>
2020-04-11 13:41:25 -0400

    Reworked to flexbox

--- src/components/sectionOne.js
+++ src/components/sectionOne.js
@@ -3,29 +3,32 @@ import { IPhoneX, Pixel3XL } from "react-device-frames";
 const questionListImg = require("../questions.png");
 const googlePlay = require('../google_play.png');
 const appStore = require('../app_store.png');
+const codeletLogoLarge = require('../logo-large-blue.png')
 export function SectionOne (){
     function scrollToBottom(){
         window.scrollTo(0,document.querySelector(".App").scrollHeight);
+    }
+    function scrollToMiddle(){
+        window.scrollTo(0,document.querySelector(".section2").scrollHeight);
     }
     return(
         <div className="section1">
             <div className="leftSection1">
                 <div className="phone-frames">
-                    <Pixel3XL screenshot={questionListImg}/>
-                    <IPhoneX screenshot={questionListImg} />
+                    <Pixel3XL className="android-phone" screenshot={questionListImg}/>
+                    <IPhoneX className="iphone" screenshot={questionListImg} />
                 </div>
                 <div className="stores">
-                    <img src={googlePlay}/>
-                    <img src={appStore}/>
+                    <img src={googlePlay} alt='google-play'/>
+                    <img src={appStore} alt='apple-store'/>
                 </div>
             </div>
-            <div className="rightSection1">
-                <div className="rightSection1Box">
-                <h1>CODELET</h1>
-                <h2>Beat Coding Interviews</h2>
-                <p>An interactive mobile platform designed to quickly prepare software engineers for technical interviews on the go.</p>
-                <button onClick={()=> scrollToBottom()}>Join the Beta</button>
-                </div>
+            <div className="rightSection1Box">
+                <img width={450} src={codeletLogoLarge} alt="logo-large"/>
+                <h2 className="headline-h2">Beat Coding Interviews</h2>
+                <p className="headline-p">An interactive mobile platform designed to quickly prepare software engineers for technical interviews on the go.</p>
+                <button className="primary-btn-section1" onClick={()=> scrollToBottom()}>Join the Beta</button>
+                <button className="secondary-btn" onClick={()=> scrollToMiddle()}>See How</button>
             </div>
         </div>
     )
