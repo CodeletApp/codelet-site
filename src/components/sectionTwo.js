76ebf2d3efaa731abea1e5d65c925f3d30078018
Mark <markgoldstein12@gmail.com>
2020-04-11 18:15:25 -0400

    Changes with jackie

--- src/components/sectionTwo.js
+++ src/components/sectionTwo.js
@@ -1,25 +1,37 @@
-import React from 'react';
-const mobile = require('../mobile.png');
-const bird = require('../bird.png');
-const analytics = require('../analytics.png');
-export function SectionTwo (){
-    return(
-        <div className="section2">
-          <div className="innerSectionBox">
-            <h1>Mobile</h1>
-            <img src={mobile}/>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
-          </div>
-          <div className="innerSectionBox">
-            <h1>Speed and Breadth</h1>
-              <img src={bird}/>
-              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
-          </div>
-          <div className="innerSectionBox">
-            <h1>Analytics</h1>
-            <img src={analytics}/>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
-          </div>
-        </div>
-    )
+import React from "react";
+const mobile = require("../mobile.png");
+const bird = require("../bird.png");
+const analytics = require("../analytics.png");
+export function SectionTwo() {
+  return (
+    <div className="section2">
+      <div className="innerSectionBox">
+        <p className="ul-title">Mobile</p>
+        <img src={mobile} />
+        <ul className="section2-list">
+          <li> On the go tagline</li>
+          <li> On the go tagline</li>
+          <li> On the go tagline</li>
+        </ul>
+      </div>
+      <div className="innerSectionBox">
+        <p className="ul-title">Speed and Breadth</p>
+        <img src={bird} />
+        <ul className="section2-list">
+          <li> On the go tagline</li>
+          <li> On the go tagline</li>
+          <li> On the go tagline</li>
+        </ul>{" "}
+      </div>
+      <div className="innerSectionBox">
+        <p className="ul-title">Analytics</p>
+        <img src={analytics} />
+        <ul className="section2-list">
+          <li> On the go tagline</li>
+          <li> On the go tagline</li>
+          <li> On the go tagline</li>
+        </ul>
+      </div>
+    </div>
+  );
 }
