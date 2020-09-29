83e3a3f246833575af80e85f5850e73a8bebf1fc
myarmolinsky <myarmolinsky3043@bths.edu>
2020-09-29 19:00:40 -0400

    Created routes for Feedback and SubmitQuestion and created Loading component

--- src/App.js
+++ src/App.js
@@ -1,19 +1,37 @@
-import React, { Component } from 'react';
+import React, { Fragment } from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import "./App.scss";
-import {SectionOne} from './components/sectionOne';
-import {SectionTwo} from './components/sectionTwo';
-import SectionThree from './components/sectionThree';
+import { Landing } from "./components/Landing";
+import { SubmitQuestion } from "./components/SubmitQuestion";
+import { Feedback } from "./components/Feedback";
 
-class App extends Component {
-  render(){
-    return (
-      <div className="App">
-        <SectionOne/>
-        <SectionTwo/>
-        <SectionThree/>
-      </div>
-    );
-  }
-}
+const App = () => {
+  return (
+    <Router>
+      <Fragment>
+        <Switch>
+          <Route exact path="/" component={Landing} />
+          <Route
+            exact
+            path="/feedback"
+            component={Feedback}
+            // component={() => {
+            //   window.location.href = "https://forms.gle/tgwrcaNJMJciME4T9";
+            // }}
+          />
+          <Route
+            exact
+            path="/submit-question"
+            component={SubmitQuestion}
+            // component={() => {
+            //   window.location.href =
+            //     "https://docs.google.com/forms/d/18EKDLIZOTYrTogTr5qoPpMAAwEkpIYdr8YorxUdLhyg/viewform?edit_requested=true";
+            // }}
+          />
+        </Switch>
+      </Fragment>
+    </Router>
+  );
+};
 
 export default App;
