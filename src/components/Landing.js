94495456c469364e4491d19e19e373e2eb329d88
myarmolinsky <myarmolinsky3043@bths.edu>
2020-10-29 10:44:56 -0400

    Removed unnecessary import from Landing

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -2,7 +2,6 @@ import React from "react";
 import { SectionOne } from "./sectionOne";
 import { SectionTwo } from "./sectionTwo";
 import SectionThree from "./sectionThree";
-import { Footer } from "./Footer";
 
 export const Landing = () => {
   return (
