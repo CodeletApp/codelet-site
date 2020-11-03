528fa5bf5925630dc5e4837e9eedf0c7bd04beec
myarmolinsky <myarmolinsky3043@bths.edu>
2020-11-03 17:40:06 -0500

    Adding styling to form

--- src/components/SubmitQuestion.js
+++ src/components/SubmitQuestion.js
@@ -63,141 +63,154 @@ export const SubmitQuestion = () => {
   };
 
   return (
-    <Fragment>
+    <div className="background">
       <CustomTextContainer>
+        <div className="title">Submit A Question</div>
+        <div className="subtitle">
+          Have an idea for a question we should add to our app? Fill out this
+          form to submit it!
+        </div>
         <form className="form" onSubmit={(e) => onSubmit(e)}>
-          <span>Author: </span>
-          <br />
-          <input
-            type="text"
-            placeholder={author}
-            name="author"
-            value={author}
-            onChange={(e) => onChange(e)}
-          />
-          <br />
-          <span>Title: </span>
-          <br />
-          <input
-            type="text"
-            placeholder={title}
-            name="title"
-            value={title}
-            onChange={(e) => onChange(e)}
-          />
-          <br />
-          <span>Difficulty: </span>
-          <br />
-          <input
-            type="text"
-            placeholder={difficulty}
-            name="difficulty"
-            value={difficulty}
-            onChange={(e) => onChange(e)}
-          />
-          <br />
-          <span>Description: </span>
-          <br />
-          <textarea
-            rows="5"
-            type="text"
-            placeholder={description}
-            name="description"
-            value={description}
-            onChange={(e) => onChange(e)}
-          />
-          <br />
-          <span>Example: </span>
-          <br />
-          <textarea
-            rows="5"
-            type="text"
-            placeholder={example}
-            name="example"
-            value={example}
-            onChange={(e) => onChange(e)}
-          />
-          <br />
-          <span>Approaches: </span>
-          <br />
-          <textarea
-            rows="5"
-            type="text"
-            placeholder={approach1}
-            name="approach1"
-            value={approach1}
-            onChange={(e) => onChange(e)}
-          />
-          <textarea
-            rows="5"
-            type="text"
-            placeholder={approach2}
-            name="approach2"
-            value={approach2}
-            onChange={(e) => onChange(e)}
-          />
-          <textarea
-            rows="5"
-            type="text"
-            placeholder={approach3}
-            name="approach3"
-            value={approach3}
-            onChange={(e) => onChange(e)}
-          />
-          <br />
-          <span>Algorithms: </span>
-          <br />
-          <textarea
-            rows="5"
-            type="text"
-            placeholder={algorithm1}
-            name="algorithm1"
-            value={algorithm1}
-            onChange={(e) => onChange(e)}
-          />
-          <textarea
-            rows="5"
-            type="text"
-            placeholder={algorithm2}
-            name="algorithm2"
-            value={algorithm2}
-            onChange={(e) => onChange(e)}
-          />
-          <textarea
-            rows="5"
-            type="text"
-            placeholder={algorithm3}
-            name="algorithm3"
-            value={algorithm3}
-            onChange={(e) => onChange(e)}
-          />
-          <br />
-          <span>Complexity Bank: </span>
-          <br />
-          <input
-            type="text"
-            placeholder={complexity1}
-            name="complexity1"
-            value={complexity1}
-            onChange={(e) => onChange(e)}
-          />
-          <input
-            type="text"
-            placeholder={complexity2}
-            name="complexity2"
-            value={complexity2}
-            onChange={(e) => onChange(e)}
-          />
-          <input
-            type="text"
-            placeholder={complexity3}
-            name="complexity3"
-            value={complexity3}
-            onChange={(e) => onChange(e)}
-          />
-          <input type="submit" />
+          <div className="form-group">
+            <label className="input-label">Author:</label>
+            <br />
+            <input
+              type="text"
+              placeholder={author}
+              name="author"
+              value={author}
+              onChange={(e) => onChange(e)}
+            />
+          </div>
+          <div className="form-group">
+            <label className="input-label">Title:</label>
+            <br />
+            <input
+              type="text"
+              placeholder={title}
+              name="title"
+              value={title}
+              onChange={(e) => onChange(e)}
+            />
+          </div>
+          <div className="form-group">
+            <label className="input-label">Difficulty:</label>
+            <br />
+            <input
+              type="text"
+              placeholder={difficulty}
+              name="difficulty"
+              value={difficulty}
+              onChange={(e) => onChange(e)}
+            />
+          </div>
+          <div className="form-group">
+            <label className="input-label">Description:</label>
+            <br />
+            <textarea
+              rows="5"
+              type="text"
+              placeholder={description}
+              name="description"
+              value={description}
+              onChange={(e) => onChange(e)}
+            />
+            <div className="form-label">Example: </div>
+            <textarea
+              rows="5"
+              type="text"
+              placeholder={example}
+              name="example"
+              value={example}
+              onChange={(e) => onChange(e)}
+            />
+          </div>
+          <div className="form-group">
+            <label className="input-label">Approaches:</label>
+            <br />
+            <textarea
+              rows="5"
+              type="text"
+              placeholder={approach1}
+              name="approach1"
+              value={approach1}
+              onChange={(e) => onChange(e)}
+            />
+            <textarea
+              rows="5"
+              type="text"
+              placeholder={approach2}
+              name="approach2"
+              value={approach2}
+              onChange={(e) => onChange(e)}
+            />
+            <textarea
+              rows="5"
+              type="text"
+              placeholder={approach3}
+              name="approach3"
+              value={approach3}
+              onChange={(e) => onChange(e)}
+            />
+          </div>
+          <div className="form-grouo">
+            <label className="input-label">Algorithms:</label>
+            <br />
+            <textarea
+              rows="5"
+              type="text"
+              placeholder={algorithm1}
+              name="algorithm1"
+              value={algorithm1}
+              onChange={(e) => onChange(e)}
+            />
+            <textarea
+              rows="5"
+              type="text"
+              placeholder={algorithm2}
+              name="algorithm2"
+              value={algorithm2}
+              onChange={(e) => onChange(e)}
+            />
+            <textarea
+              rows="5"
+              type="text"
+              placeholder={algorithm3}
+              name="algorithm3"
+              value={algorithm3}
+              onChange={(e) => onChange(e)}
+            />
+          </div>
+          <div className="form-group">
+            <label className="input-label">Complexity Bank:</label>
+            <br />
+            <input
+              type="text"
+              placeholder={complexity1}
+              name="complexity1"
+              value={complexity1}
+              onChange={(e) => onChange(e)}
+            />
+            <input
+              type="text"
+              placeholder={complexity2}
+              name="complexity2"
+              value={complexity2}
+              onChange={(e) => onChange(e)}
+            />
+            <input
+              type="text"
+              placeholder={complexity3}
+              name="complexity3"
+              value={complexity3}
+              onChange={(e) => onChange(e)}
+            />
+          </div>
+          <div className="form-group">
+            <input type="submit" />
+          </div>
         </form>
       </CustomTextContainer>
-    </Fragment>
+    </div>
   );
 };
