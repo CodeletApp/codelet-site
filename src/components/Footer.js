3506dc9214eb506ee98d280f6a24a71abaee7367
maker-mark <markgoldstein12@gmail.com>
2021-05-16 18:35:02 -0400

    update footer content

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,16 +4,24 @@ import { Link } from "react-router-dom";
 export function Footer() {
   return (
     <div className="footer">
-      <Link className="footer-link" to="/">
+      <Link className="footer__link" to="/">
         Home
       </Link>
-      <Link className="footer-link" to="/terms">
+      <Link className="footer__link" to="/terms">
         Terms & Conditions
       </Link>
-      <Link className="footer-link" to="/privacy">
+      <div className="footer__codelet">
+        <img
+          className="logo"
+          src="/logos/codelet-c.png"
+          alt="Large Codelet Logo"
+        />
+        <p>Codelet 2021 </p>
+      </div>
+      <Link className="footer__link" to="/privacy">
         Privacy Policy
       </Link>
-      <Link className="footer-link" to="/feedback">
+      <Link className="footer__link" to="/feedback">
         Leave Feedback
       </Link>
     </div>
