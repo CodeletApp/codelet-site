ad9dc78470f4a4b19efa029a9fcbaeec24860c25
Mark Goldstein <markgoldstein12@gmail.com>
2022-01-15 13:43:26 -0500

    Remove section 3

--- src/pages/Landing.js
+++ src/pages/Landing.js
@@ -1,14 +1,12 @@
 import React from "react";
 import { SectionOne } from "../components/sectionOne";
 import { SectionTwo } from "../components/sectionTwo";
-import SectionThree from "../components/sectionThree";
 
 export const Landing = () => {
   return (
     <div className="App">
       <SectionOne />
       <SectionTwo />
-      {/* <SectionThree /> */}
     </div>
   );
 };
